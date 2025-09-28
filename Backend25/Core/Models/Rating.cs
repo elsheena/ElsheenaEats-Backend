19a906694876f3e31f82cf64c918f95442232d6f
elsheena <monmonmisho26@gmail.com>
2025-09-28 19:28:20 +0700

    Added Rating and Order classes

---
+++ Backend25/Core/Models/Rating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Rating
+    {
+        public Guid Id { get; set; }
+        public int Value { get; set; }
+        public Guid DishId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public DateTime ModifyDateTime { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        public User User { get; set; } = null!;
+        public Dish Dish { get; set; } = null!;
+    }
+}
